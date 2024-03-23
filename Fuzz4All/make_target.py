b800b504e6efce7531bd4e93a6c679bff8d529e6
Xinshuo Lei <leixshuo@gmail.com>
2024-03-22 21:14:45 -0500

    update

--- Fuzz4All/make_target.py
+++ Fuzz4All/make_target.py
@@ -1,6 +1,32 @@
 from typing import Any, Dict, List, Optional, Tuple, Union
-from Fuzz4All.target.target import Target
+
+from Fuzz4All.target.C.C import CTarget
+from Fuzz4All.target.CPP.CPP import CPPTarget
+from Fuzz4All.target.GO.GO import GOTarget
+from Fuzz4All.target.JAVA.JAVA import JAVATarget
+from Fuzz4All.target.QISKIT.QISKIT import QiskitTarget
+from Fuzz4All.target.SMT.SMT import SMTTarget
 from Fuzz4All.target.JSHERMES import JSHERMESTarget
+from Fuzz4All.target.target import Target
+
+
+def make_target(kwargs: Dict[str, Any]) -> Target:
+    """Make a target from the given command line arguments."""
+    language = kwargs["language"]
+    if language == "cpp":  # G++
+        return CPPTarget(**kwargs)
+    elif language == "c":  # GCC
+        return CTarget(**kwargs)
+    elif language == "qiskit":  # Qiskit
+        return QiskitTarget(**kwargs)
+    elif language == "smt2":  # SMT solvers
+        return SMTTarget(**kwargs)
+    elif language == "go":  # GO
+        return GOTarget(**kwargs)
+    elif language == "java":  # Java
+        return JAVATarget(**kwargs)
+    else:
+        raise ValueError(f"Invalid target {language}")
 
 
 def make_target_with_config(config_dict: Dict[str, Any]) -> Target:
@@ -36,8 +62,19 @@ def make_target_with_config(config_dict: Dict[str, Any]) -> Target:
         print(f"{k}: {v}")
     print("====================")
 
-
-    if target["language"] == "js-hermes":
+    if target["language"] == "cpp":
+        return CPPTarget(**target_compat_dict)
+    elif target["language"] == "c":
+        return CTarget(**target_compat_dict)
+    elif target["language"] == "qiskit":
+        return QiskitTarget(**target_compat_dict)
+    elif target["language"] == "smt2":
+        return SMTTarget(**target_compat_dict)
+    elif target["language"] == "go":
+        return GOTarget(**target_compat_dict)
+    elif target["language"] == "java":
+        return JAVATarget(**target_compat_dict)
+    elif target["language"] == "js-hermes":
         return JSHERMESTarget(**target_compat_dict)
     else:
         raise ValueError(f"Invalid target {target['language']}")
